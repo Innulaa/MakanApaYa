9d879e1263226e83769f664cf8a72f1af675a6e2
Intan Nur Laila <149084934+Innulaa@users.noreply.github.com>
2025-06-14 22:04:17 +0700

    logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-session_unset(); // Menghapus semua variabel session
-session_destroy(); // Menghancurkan session
+session_unset(); 
+session_destroy(); 
 
-// Arahkan ke halaman index
 header("Location: index.php");
 exit;
 ?>
